3c383765ffe4268730d5f43fc2de5c383d956f56
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-10-01 21:01:12 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,12 +5,12 @@ function Todo({todos,deleteTask}){
   return(
     <ul className="todo-list">
     {
-      todos.map((todo,index)=>{
+      todos.map((todo,index)=>(
         <li key={index} className="todo-item">
                 {todo}
                 <button onClick={()=>deleteTask(index)} className="delete-btn">Delete</button>
         </li>
-      })
+      ))
     }
     </ul>
   )
